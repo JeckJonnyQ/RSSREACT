138e5498b065a0a88a7cc0057c7b65c88688c559
Reut Vladislav <reut-2011@mail.ru>
2024-07-05 01:34:19 +0300

    feat: settings lint-stage

--- CLASS-COMPONENTS/src/main.tsx
+++ CLASS-COMPONENTS/src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
+
+const asd: any = 0;
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
